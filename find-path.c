104410170b1ce61e0078b47170d9df6b0fa467be
GraceKayem <11557@holbertonstudents.com>
2025-08-21 15:54:42 +0000

    save

---
+++ find-path.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include "shell.h"
+
+extern char **environ;
+/**
+ * find_command - Simple shell 0.2
+ * @command: Handle the PATH
+ * Return: malloc'd full path if found (caller must free), or NULL if not found
+ */
+char *find_command(char *command)
+{
+    int length;
+    char *path, *path_copy, *full_path, *directory;
+    
+    if (command == NULL)
+    {
+       return (NULL);
+    }
+   
+   /*checking the first index of a command -- Absolute vs relative path check*/
+     if (command[0] == '/' || strncmp(command, "./", 2) == 0)
+     {
+        if (access(command, X_OK) == 0)
+        {
+            return (strdup(command));
+	}
+	return (NULL);
+     }
+     
+    /* Get PATH */
+    path = getenv("PATH");
+    if (path == NULL)
+    {
+        return (NULL);
+    }
+    
+    /*making a copy of the path to avoid affecting the path directly*/
+    path_copy = strdup(path);
+    /*checking if path has been defined*/
+    if (path_copy == NULL)
+    {
+        return (NULL);
+    }
+    
+    /*Tokenizing*/
+    directory = strtok(path_copy, ":");
+    while (directory != NULL)
+    {
+        /*determine the length of the directory*/
+        length = strlen(directory) + strlen (command) + 2;
+        full_path = malloc(sizeof(char) * length);
+        if (full_path == NULL)
+        {
+            free(path_copy);
+            return (NULL);
+        }
+        
+        snprintf(full_path, length, "%s/%s", directory, command);
+        
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return (full_path);
+        }
+        
+        free(full_path);
+        /*Tokenizing*/
+        directory = strtok(NULL, ":");
+    }
+    free(path_copy);
+    /*PATH not found*/
+    return (NULL); 
+}
+
+/**
+ * execute_command - fork must not be called if the command doesn’t exist
+ * @command: command name
+ * @args: argument vector (argv)
+ * Return: 0 if success, 1 if failure
+ */
+int handle_command(char *command, char **args)
+{
+    char *full_path;
+    pid_t pid;
+    
+    full_path = find_command(command);
+    if (full_path == NULL)
+    {
+        fprintf(stderr, "%s: Command not found\n", command);
+        return (1);
+    }
+
+    pid = fork();
+    if (pid == -1) 
+    {
+        perror("Error:");
+        free(full_path);
+        return (1);
+    }
+    
+    if (pid == 0) 
+    {
+        execve(full_path, args, environ);
+        perror("execve");
+        exit(1);
+    }
+    else
+    {
+        wait(NULL);
+    }
+    
+    free(full_path);
+    return (0);
+}
---
+++ simple-shell.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include "shell.h"
+#include "builtins.h"
+
+/**
+ * only_spaces - searches a string to check for only_spaces
+ * @line: string to check
+ * Return: 1 if line is only spaces or tabs, 0 if other characters found
+ */
+int only_spaces(char *line)
+{
+	int i = 0;
+
+	while (line[i] != '\0')
+	{
+		if (line[i] != ' ' && line[i] != '\t')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - entrypoint to simple shell
+ * @ac: Number of args passed to the program
+ * @av: Array of arguments
+ * Return: 0
+ */
+extern char **environ;
+
+int main(int ac, char **av)
+{
+	char *line;
+	size_t buffer_size = 256;
+	ssize_t nread;
+	int child;
+	int tty = 1, exec_return = 0;
+	int builtin_status = 0;
+	(void)ac;
+	line = malloc(buffer_size + 1);
+	tty = isatty(STDIN_FILENO);
+
+	while (1)
+	{
+		if (tty == 1)
+			printf("#cisfun$: ");
+		nread = getline(&line, &buffer_size, stdin);
+		
+		if (nread == -1)
+		{
+			break;
+		}
+		line[strcspn(line, "\n")] = '\0';
+		if (line[0] == ' ')
+		{
+			if (only_spaces(line) == 1)
+				break;
+		}
+		av = tokenize_line(line);
+		builtin_status = handle_builtin(av);
+
+		if (builtin_status == -1)
+		{
+			free_argv(av);
+			free(line);
+			exit(0);
+		}
+		else if (builtin_status == 1)
+		{
+			free_argv(av);
+			continue;
+		}
+
+		child = fork();
+
+		if (child < 0)
+		{
+			perror("Fork failed");
+			exit(EXIT_FAILURE);
+		}
+		else if (child == 0)
+		{
+			exec_return = execve(av[0], av, environ);
+			if (exec_return == -1)
+			{
+				perror("./shell");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			int status;
+			wait(&status);
+
+			if (WIFEXITED(status))
+				exec_return = WEXITSTATUS(status);
+			else
+				exec_return = 1;
+		}
+
+		free_argv(av);
+		if (exec_return == 2)
+		{
+			free(line);
+			return (exec_return);
+		}
+	}
+
+	free(line);
+	return (exec_return);
+}
